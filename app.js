b6b6db55f1bc7ea91ff8b023969fe2b6afcf748e
unknown <chandrapratap.solanki@gmail.com>
2020-02-16 01:45:04 +0530

    alert function

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ GAME RULES:
 
 */
 
+alert(`GAME RULES:
+
+    - The game has 2 players, playing in rounds
+    - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+    - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+    - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+    - The first player to reach 100 points on GLOBAL score wins the game
+    `);
+
 var  scores, roundScore, activePlayer, gamePlaying ;
 
 init();
@@ -79,15 +88,7 @@ function nextPlayer(){
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
-function init(){
-    alert(`GAME RULES:
-
-    - The game has 2 players, playing in rounds
-    - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-    - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-    - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-    - The first player to reach 100 points on GLOBAL score wins the game
-    `);
+function init(){    
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
